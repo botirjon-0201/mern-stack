e59bad93875f3139d71550cd10970a24567dc8da
Botirjon <botirjonumurzoqov02.01.1991@gmail.com>
2023-02-02 10:28:48 +0500

    fixed some bags

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 5000;
 const { MONGO_URI } = require("./keys");
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 require("./models/user");
 require("./models/post");
@@ -13,7 +13,10 @@ app.use(require("./routes/post"));
 app.use(require("./routes/user"));
 
 mongoose.set("strictQuery", false);
-mongoose.connect(MONGO_URI);
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongDB.."))
+  .catch((err) => console.error("Error connecting to MongDB...", err));
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}...`);
--- server/keys/index.js
+++ server/keys/index.js
@@ -1,5 +1,6 @@
 module.exports = {
-  MONGO_URI:
-    "mongodb+srv://Botirjon:m8gCJT0oThDPs5ht@cluster0.edxewqr.mongodb.net/sammigram",
+  MONGO_URI: "mongodb://localhost/demogramm",
+  // MONGO_URI:
+  //   "mongodb+srv://Botirjon:m8gCJT0oThDPs5ht@cluster0.edxewqr.mongodb.net/sammigram",
   JWT_SECRET: "chunpul"
 };
